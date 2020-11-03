1ca14c6816334672e812fb9dfe3815a6ed247469
Paul Elliott <paul@scalefactory.com>
2020-11-03 14:06:19 +0000

    No need to specify format on decompression

--- src/LayerCache.ts
+++ src/LayerCache.ts
@@ -50,7 +50,7 @@ class LayerCache {
 
   private async saveImageAsUnpacked() {
     await fs.mkdir(this.getSavedImageTarDir(), { recursive: true })
-    await this.exec(`sh -c`, [`docker save '${(await this.makeRepotagsDockerSaveArgReady(this.ids)).join(`' '`)}' | tar -x --gzip -f - -C .`], { cwd: this.getSavedImageTarDir() })
+    await this.exec(`sh -c`, [`docker save '${(await this.makeRepotagsDockerSaveArgReady(this.ids)).join(`' '`)}' | tar -x -f - -C .`], { cwd: this.getSavedImageTarDir() })
   }
 
   private async makeRepotagsDockerSaveArgReady(repotags: string[]): Promise<string[]> {
